20ffca4e02fc9c8ef34c6818566adcda41d23cf3
martinness <martin.ness@icloud.com>
2023-01-10 20:29:10 +0100

    Initial commit

---
+++ c_practice1.cpp
@@ -0,0 +1,13 @@
+// what's so cool about C++:
+// Object oriented code
+// Popular and fast 
+// All programs starts with int main(), { return X; } 
+
+#include <iostream> //Input/Output stream, preprocessed information, including cout
+int main() //main function
+{
+	std::cout << "Hello World\n"; //output to the consol
+	return 0;
+}
+
+
