19e23934fa0ddb7c82071fef5eac2f5f5654ea64
martinness <martin.ness@icloud.com>
2023-01-27 12:46:14 +0100

    Updating with additional lines from turtorial

--- c_practice1.cpp
+++ c_practice1.cpp
@@ -1,21 +1,30 @@
 #include <iostream> //Input/Output stream, preprocessed information, including cout
 #include <cmath>
+#include <vector>
+#include <string>
+#include <fstream>
+
 using namespace std;
 
-int main() //main function
-{
+int main(){
 	cout << "Enter a radius of the circle ";
 	double radius; 
 	/*
 	calculates the area of a circle
+
 	*/ 
 	cin >> radius; 
-	double area;
-	area = M_PI*pow(radius, 2);
-	cout << "the area of the circle is " << area << endl;
+	char myGrade = 'A';
+	bool isHappy = true;
+	int myAge =29;
+	float favNum =3.141592;
+	double otherfavNum= 1.618033988;
+
+	auto  area =  M_PI*pow(radius, 2);
+	cout << "the AREA of the circle is " << area << endl;
+	cout << "size of int" << sizeof(area) << endl;
+
 	return 0;
-
-
 }
 
 
